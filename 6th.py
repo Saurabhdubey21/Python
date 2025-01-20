b98bc4ae8c4186848b5b7b6a00765415ec72ab69
Saurabhdubey21 <mkd221524@gmail.com>
2025-01-20 22:02:51 +0530

    find interest

---
+++ 6th.py
@@ -0,0 +1,5 @@
+a=float(input("Enter principal amount:"))
+b=float(input("Enter a months number:"))
+c=float(input("Enter rate of interest:%10",))
+interest=(a*b*c)/100
+print("interest is:",interest)
