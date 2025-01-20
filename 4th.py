f2c35940ee6cd1bc527d929e524142c82372bfc9
Saurabhdubey21 <mkd221524@gmail.com>
2025-01-20 21:42:43 +0530

    Two numbers swapping

---
+++ 4th.py
@@ -0,0 +1,7 @@
+a=int(input("Enter any number:"))
+b=int(input("Enter any number:"))
+print("Before Swapping numbers are:",a,b)
+temp=a
+a=b
+b=temp
+print("after swapping the value of a and b:",a,b)
