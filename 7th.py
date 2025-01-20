0e73b2b431b3ec8a0a07dc830ecb975980fcff4e
Saurabhdubey21 <mkd221524@gmail.com>
2025-01-20 22:08:48 +0530

    find surface area of cylinder

---
+++ 7th.py
@@ -0,0 +1,5 @@
+import math
+radius=float(input("Enter the radius:"))
+height=float(input("Enter the height:"))
+surface_area=2*math.pi*radius*(radius+height)
+print("Surface area of cylinder:",surface_area)
