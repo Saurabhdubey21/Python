1f347087cb563315dc519f70caa18085672f072a
Saurabhdubey21 <mkd221524@gmail.com>
2025-01-20 21:54:09 +0530

    temerature conversion

---
+++ 5th.py
@@ -0,0 +1,6 @@
+a=float(input("Enter a temperature in fareheit:"))
+c=(a-32)*5/9
+print("The temperature will:",c,"c degree")
+b=float(input("Enter the temperature in celcius:"))
+d=(b*1.8)+32
+print("temperature will:",d,"f degree")
