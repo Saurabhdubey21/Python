65ad1e4ce66fad03359fee01b06b11da6240f472
Saurabhdubey21 <mkd221524@gmail.com>
2025-01-21 22:06:50 +0530

    Arithmetic operation problem

---
+++ day2/6th.py
@@ -0,0 +1,16 @@
+a=int(input("Enter the number: "))
+b=int(input("Enter the number: "))
+sum=a+b
+print("Sum of two numbers is: ",sum)
+sub=a-b
+print("Subtraction of two numbers is: ",sub)
+mul=a*b
+print("Multiplication of two numbers is: ",mul)
+div=a/b
+print("Division of two numbers is: ",div)
+mod=a%b
+print("Modulus of two numbers is: ",mod)
+exp=a**b
+print("Exponential of two numbers is: ",exp)
+floor=a//b
+print("Floor division of two numbers is: ",floor)
