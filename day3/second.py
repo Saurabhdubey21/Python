eb74c459568c5849d7741903adae6b10cfa41576
Saurabhdubey21 <mkd221524@gmail.com>
2025-01-22 23:03:25 +0530

    string operations in python

---
+++ day3/second.py
@@ -0,0 +1,13 @@
+#string operations
+s1="hello"
+print(s1)
+s2="world"
+print(s2)
+s3='''This is a multi-line string'''
+print(s3)
+print(s1[0])
+print(s2[0])
+print(s3[0])
+print(s1[4])
+print(s2[3])
+print(s3[5])
