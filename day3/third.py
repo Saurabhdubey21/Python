eb556e172125a9887338080cd7ccd7bad5fafbfe
Saurabhdubey21 <mkd221524@gmail.com>
2025-01-22 23:07:26 +0530

    string slicing in python

---
+++ day3/third.py
@@ -0,0 +1,13 @@
+#string slicing{strat, end, step}
+s1="Hello this is a string"
+s2="Python is a programming language"
+print(s1)
+print(s2)
+print(s1[0:5])
+print(s2[0:6])
+print(s1[0:5:2])
+print(s2[0:6:2])
+print(s1[0:5:3])
+print(s2[0:6:3])
+print(s1[0:5:4])
+print(s2[:4])
