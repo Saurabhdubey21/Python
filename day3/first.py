4064cab68d0dd1a17c8b411069f2e05e9e227e28
Saurabhdubey21 <mkd221524@gmail.com>
2025-01-22 22:56:15 +0530

    expression contains the commit message

---
+++ day3/first.py
@@ -0,0 +1,17 @@
+#Expression constants
+result = 5+9
+print(result)
+a=5
+b=9
+print(a+b)
+print(a or b)
+print(a and b)
+print(a>b)
+print(a<b)
+print(a&b)
+print(a|b)
+print("Hello World"+"Python is a programming language")
+str1="Hello World"
+str2="Python is a programming language"
+str3=str1+str2
+print(str3)
