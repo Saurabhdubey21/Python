cda71b3d2382192a40c3e4ff06aac88d4ac6fac5
Saurabhdubey21 <mkd221524@gmail.com>
2025-01-23 20:37:49 +0530

    string list operation in python

---
+++ day3/sixth.py
@@ -0,0 +1,9 @@
+#string list operation in python
+my_list = [1,2,3,4,5,'Hello',4,5,6,1,2,3]
+print(my_list)
+my_tuple = (1,2,3,4,5,'Hello',4,5,6,1,2,3)
+print(my_tuple)
+my_set = {1,2,3,4,5,'Hello',4,5,6,1,2,3}
+print(my_set)
+my_dict = {1:'Hello',2:'World',3:'Python',4:'Programming',5:'Language'}
+print(my_dict)
