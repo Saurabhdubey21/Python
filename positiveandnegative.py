dae52eee6b2637b39d02ef0bd87017c411b5f808
Saurabhdubey21 <mkd221524@gmail.com>
2025-01-24 23:11:07 +0530

    find positive and negative number

---
+++ positiveandnegative.py
@@ -0,0 +1,10 @@
+#find positive numbers negative numbers and zeros in a list
+n=int(input("Enter any number: "))
+print(n)
+if n>0:
+    print("The number is positive")
+else:
+    if n<0:
+        print("The number is negative")
+    else:
+        print("The number is zero")
