2d9334d6013dfba6155fd168ce0fbabaac76db8f
Saurabhdubey21 <mkd221524@gmail.com>
2025-01-25 21:29:29 +0530

    loop in horizontal and vertical

---
+++ differentloop.py
@@ -0,0 +1,10 @@
+#loop in horizontal direction
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(i, end=" ")
+print()
+#loop in vertical direction
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)
+print()
