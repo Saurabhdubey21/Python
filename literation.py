745c69eecf6214432bd2c3eff762993c51d0a928
Saurabhdubey21 <mkd221524@gmail.com>
2025-01-26 23:12:43 +0530

    literation over list in python

---
+++ literation.py
@@ -0,0 +1,16 @@
+#iteration over a list
+fruits=['apple','banana','cherry']
+for fruit in fruits:
+    print(fruits)
+#iteration over a string
+for letter in 'banana':
+    print(letter)
+#iteration over a range
+for i in range(5):
+    print(i)
+#iteration over a list of numbers
+for i in [1,2,3,4,5]:
+    print(i)
+#iteration over a list of tuples
+for i in [(1,2),(3,4),(5,6)]:
+    print(i)
