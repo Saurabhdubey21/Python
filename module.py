5fb7f7234b1203341418acbedefc2cb26aa3129d
Saurabhdubey21 <mkd221524@gmail.com>
2025-01-30 23:05:00 +0530

    FIRST METHOD TO IMPORT MODULE

---
+++ module.py
@@ -0,0 +1,19 @@
+# FIRST METHOD TO IMPORT MODULE
+import math
+result=math.sqrt(12)
+print(result)
+# SECOND METHOD TO IMPORT MODULE
+from math import sqrt
+result=sqrt(12)
+print(result)
+# THIRD METHOD TO IMPORT MODULE
+from math import *
+# result=sqrt(12)
+print(result)
+# FOURTH METHOD TO IMPORT MODULE
+from math import *
+result=sqrt(12)
+print(result)
+print(pi)
+print(result)
+print(sin(0))
