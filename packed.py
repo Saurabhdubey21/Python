5e19bc6db59b2f9e9af71d39a2ab03fe90c666d2
Saurabhdubey21 <mkd221524@gmail.com>
2025-01-31 23:24:24 +0530

    packing the code

---
+++ packed.py
@@ -0,0 +1,13 @@
+# sequences
+# string
+# list
+# tuple
+# packing
+packed=1,2,3,4,5,6,7
+print(packed)
+# unpacking
+x,y,z=1,2,3
+print(x,y,z) 
+# using * to collect remaining items
+*a ,b=[1,2,3]
+print(a,b)
