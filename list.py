8edf3ec368742e6d6dcd759b4543fef52961fb3e
Saurabhdubey21 <mkd221524@gmail.com>
2025-01-31 23:37:55 +0530

    list operations added

---
+++ list.py
@@ -0,0 +1,4 @@
+my_list=[1,2,3,4,5,6]
+my_list.append(6)
+my_list[1]=7
+print(my_list)
--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-from mymodule import add,PI
-# num1=int(input("Enter any number:"))
-# num2=int(input("Enter any number:"))
-# print(mymodule.add(num1,num2))
-# print(PI)
