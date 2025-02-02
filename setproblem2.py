3d80119598e11d3e4dce70de36d85ca2b816ebd4
Saurabhdubey21 <mkd221524@gmail.com>
2025-02-02 22:05:20 +0530

    set and dict problems

---
+++ setproblem2.py
@@ -0,0 +1,10 @@
+set_1={1,2,3,}
+print(set_1)
+set_2 ={1,2,3,4,5}
+print(set_2)
+print(set_1.union(set_2))
+print(set_1.intersection(set_2))
+print(set_1.difference(set_2))
+print(set_1.symmetric_difference(set_2))
+print(set_1.issuperset(set_2))
+print(set_1.issubset(set_2))
