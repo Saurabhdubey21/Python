668cb2307ad52d1115303f48b5b465bb216ec5e0
Saurabhdubey21 <mkd221524@gmail.com>
2025-02-28 00:49:23 +0530

    addition of matrices

---
+++ additionofmatrices.py
@@ -0,0 +1,15 @@
+#addition of two matrices row and columb wise in python
+matrix1 = [[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]]
+matrix2 = [[9, 8, 7],
+                [6, 5, 4],
+                [3, 2, 1]]
+result = [[0, 0, 0],
+              [0, 0, 0],
+              [0, 0, 0]]
+for i in range(len(matrix1)):
+    for j in range(len(matrix1[0])):
+        result[i][j] = matrix1[i][j] + matrix2[i][j]
+for r in result:
+    print(r)
